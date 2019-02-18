4752754423064acc4628c74bddf32865cf089021
Jesper Alfraeus <jeal@netlight.com>
2019-02-18 22:12:44 +0100

    Change backgound

--- art-by-karin/src/App.js
+++ art-by-karin/src/App.js
@@ -9,8 +9,9 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Välkommen raring. 
           </p>
+          <p><code>src/App.js</code></p>
           <a
             className="App-link"
             href="https://reactjs.org"
